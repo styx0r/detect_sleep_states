e6a00a9326f1177c8d95b65d5a9bf8da9976893d
Christoph Baldow <christoph.baldow@gmail.com>
2023-09-13 22:48:03 +0200

    adds df conversion to more efficient version

---
+++ detect_sleep_states/run.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import gc
+
+train_series = pd.read_parquet('../data/train_series.parquet')
+train_events = pd.read_csv('../data/train_events.csv')
+
+
+######### Transform data to memory efficient representation #########
+# series_id mapping
+series_ids = pd.DataFrame({'series_id': train_series.series_id.unique()})
+mapping_series_ids = series_ids.reset_index().set_index('series_id')['index'].to_dict()
+train_series['series_id'] = train_series['series_id'].map(mapping_series_ids)
+train_series['series_id'] = train_series['series_id'].astype('uint16')
+
+train_series['step'] = train_series['step'].astype('uint32')
+
+train_series['year']=train_series['timestamp'].str[:4].astype('uint16')
+years = pd.DataFrame({'year': sorted(train_series.year.unique())})
+mapping_years = years.reset_index().set_index('year')['index'].to_dict()
+train_series['year'] = train_series['year'].map(mapping_years)
+train_series['year'] = train_series['year'].astype('uint8')
+
+train_series['month']=train_series['timestamp'].str[5:7].astype('uint8')
+train_series['day']=train_series['timestamp'].str[8:10].astype('uint8')
+train_series['seconds']=train_series['timestamp'].str[11:13].astype('uint32') * 60 * 60 + train_series['timestamp'].str[14:16].astype('uint32') * 60 + train_series['timestamp'].str[17:19].astype('uint32')
+
+# drop timestamp and free memory
+train_series.drop('timestamp', axis=1, inplace=True)
+gc.collect()
