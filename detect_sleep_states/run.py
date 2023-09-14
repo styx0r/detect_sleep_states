ef8996629b85ec08559788ed2f442aef1113ff24
Christoph Baldow <christoph.baldow@gmail.com>
2023-09-14 12:04:44 +0200

    refactoring dtype conversions

--- detect_sleep_states/run.py
+++ detect_sleep_states/run.py
@@ -1,34 +1,54 @@
 import pandas as pd
+import numpy as np
 import gc
 
+from joblib import Parallel, delayed
+import multiprocessing
+
 import datetime
+
+from tqdm import tqdm
+tqdm.pandas()
+
+def add_mapping(mappings, df: pd.DataFrame, name_col: str):
+    unique_mappings = pd.DataFrame({name_col: sorted(df[name_col].unique())})
+    mappings[name_col] = unique_mappings.reset_index().set_index(name_col)['index'].to_dict()
+
+def map_and_convert_type(data: pd.Series, mapping_dict: dict, dtype: str):
+    return data.map(mapping_dict).astype(dtype)
+
+def extract_timestamp_features(df: pd.DataFrame, timestamp_col: str = 'timestamp', drop_timestamp_col: bool = True):
+    df['seconds']=df[timestamp_col].str[11:13].astype('uint32') * 60 * 60 + df[timestamp_col].str[14:16].astype('uint32') * 60 + df[timestamp_col].str[17:19].astype('uint32')
+    df['day']=df[timestamp_col].str[8:10].astype('uint8')
+    df['month']=df[timestamp_col].str[5:7].astype('uint8')
+
+    df['year']=df[timestamp_col].str[:4].astype('uint16')
+
+    if drop_timestamp_col:
+        # drop timestamp and free memory
+        df.drop(timestamp_col, axis=1, inplace=True)
+        gc.collect()
+
+    def weekday_parallel(chunk):
+        tqdm.pandas()
+        return chunk[['year', 'month', 'day']].progress_apply(lambda x:  datetime.date(x['year'], x['month'], x['day']).weekday(), axis=1)
+    df['weekday'] = pd.concat(Parallel(n_jobs=-1)(delayed(weekday_parallel)(chunk) for chunk in np.array_split(df[['year', 'month', 'day']], multiprocessing.cpu_count() - 1))).astype('uint8')    
+
 
 train_series = pd.read_parquet('../data/train_series.parquet')
 train_events = pd.read_csv('../data/train_events.csv')
 
+# mappings storage
+mappings = {}
 
 ######### Transform data to memory efficient representation #########
 # series_id mapping
-series_ids = pd.DataFrame({'series_id': train_series.series_id.unique()})
-mapping_series_ids = series_ids.reset_index().set_index('series_id')['index'].to_dict()
-train_series['series_id'] = train_series['series_id'].map(mapping_series_ids)
-train_series['series_id'] = train_series['series_id'].astype('uint16')
+add_mapping(mappings, train_series, 'series_id')
+train_series['series_id'] = map_and_convert_type(train_series['series_id'], mappings['series_ids'], 'uint16')
 
 train_series['step'] = train_series['step'].astype('uint32')
 
-train_series['seconds']=train_series['timestamp'].str[11:13].astype('uint32') * 60 * 60 + train_series['timestamp'].str[14:16].astype('uint32') * 60 + train_series['timestamp'].str[17:19].astype('uint32')
-train_series['day']=train_series['timestamp'].str[8:10].astype('uint8')
-train_series['month']=train_series['timestamp'].str[5:7].astype('uint8')
+extract_timestamp_features(train_series)
 
-train_series['year']=train_series['timestamp'].str[:4].astype('uint16')
-
-# drop timestamp and free memory
-train_series.drop('timestamp', axis=1, inplace=True)
-gc.collect()
-
-train_series['weekday']=train_series.apply(lambda x:  datetime.date(x['year'], x['month'], x['day']).weekday(), axis=1)
-
-years = pd.DataFrame({'year': sorted(train_series.year.unique())})
-mapping_years = years.reset_index().set_index('year')['index'].to_dict()
-train_series['year'] = train_series['year'].map(mapping_years)
-train_series['year'] = train_series['year'].astype('uint8')
+add_mapping(mappings, train_series, 'year')
+train_series['year'] = map_and_convert_type(train_series['year'], mappings['year'], 'uint8')
